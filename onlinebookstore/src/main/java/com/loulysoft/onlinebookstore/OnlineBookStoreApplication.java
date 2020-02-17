38797e33e91844b2e1dbd217d2fba524d2d6e353
a780579 <a780579@LAPTOP-TQ4SVTVL>
2020-02-17 16:43:30 +0000

    Initial commit : Created spring boot project and configure the application properties file

--- onlinebookstore/src/main/java/com/loulysoft/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/src/main/java/com/loulysoft/onlinebookstore/OnlineBookStoreApplication.java
@@ -2,7 +2,9 @@ package com.loulysoft.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Main application class
+ */
 @SpringBootApplication
 public class OnlineBookStoreApplication {
 
